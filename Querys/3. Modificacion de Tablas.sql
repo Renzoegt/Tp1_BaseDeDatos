f611017c11183238e0282024fe2e236c841ad777
Yume <150016933+Renzoegt@users.noreply.github.com>
2025-09-05 22:36:26 -0300

    avances

--- Querys/3. Modificacion de Tablas.sql
+++ Querys/3. Modificacion de Tablas.sql
@@ -3,3 +3,9 @@ Específicamos que schema estamos usando*/
 use aerolinea_ez;
 
 /*Agregamos una columna telefono a la tabla pasajero despues de la columna nro_documento*/
+alter table pasajero add telefono varchar(20) not null;
+
+/*modificamos el tipo de dato de monto en la tabla reserva*/
+alter table reserva modify monto smallint not null;
+
+/**/

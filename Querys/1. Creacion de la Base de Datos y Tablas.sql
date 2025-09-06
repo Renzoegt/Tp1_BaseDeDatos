16453e07a0f4f7df656a21c4316f6efa550c60b4
Yume <150016933+Renzoegt@users.noreply.github.com>
2025-09-05 21:07:53 -0300

    arreglo de 1

--- Querys/1. Creacion de la Base de Datos y Tablas.sql
+++ Querys/1. Creacion de la Base de Datos y Tablas.sql
@@ -16,14 +16,14 @@ create table pasajero(
     telefono varchar(20) not null,
     email varchar(60) not null,
     tipo_documento varchar(15) not null,
-    nro_documento int(20) not null,
+    nro_documento smallint not null,
     fecha_registro datetime default current_timestamp,
     constraint chk_documento check (tipo_documento in ('dni','cuil', 'pasaporte'))
 );
 
 /*Creamos la tabla vuelo. Como los tiempos de salida no son subidos en el momento de la salida per se no lo seteamos 
 por default al momento de creacion del vuelo. El combustible lo seteamos como varchar para poder agregar lts al final
-del numero. Creamos una relación con la tabla avión en la que el vuelo tendrá el id del avion que realiza ese vuelo*/
+del numero. No creamos una relación con la tabla avión porque como todavia no existe nuestra query no va a funcionar*/
 create table vuelo(
 	vuelo_id int not null auto_increment primary key,
     avion_id int not null,
@@ -32,8 +32,7 @@ create table vuelo(
     fecha_salida datetime not null,
     fecha_estimada_arribo datetime not null,
     combustible varchar(10) not null default 5,
-    capacidad smallint not null default 5,
-    foreign key(avion_id) references avion(avion_id)
+    capacidad smallint not null default 5
 );
 
 /*creamos la tabla reserva añadimos como default en forma de pago transferencia bancaria y chequeamos que los valores sean ese, 
@@ -47,10 +46,15 @@ create table reserva(
     forma_pago varchar(30) not null default 'transferencia bancaria',
     foreign key(pasajero_id) references pasajero(pasajero_id),
     foreign key(vuelo_id) references vuelo(vuelo_id),
-	constraint chk_fp check (tipo_documento in ('efectivo','tarjeta de credito', 'transferencia bancaria'))
+	constraint chk_fp check (forma_pago in ('efectivo','tarjeta de credito', 'transferencia bancaria'))
 );
 
 create table avion(
 	avion_id int not null auto_increment primary key,
-    
+    vuelo_id int not null,
+    marca varchar(20) not null,
+    modelo varchar(20) not null,
+    anio_contratacion smallint not null,
+    activo boolean default true, /*Creamos la variable activo, poniendola como booleano. true=activo, false=inactivo*/
+    foreign key (vuelo_id) references vuelo(vuelo_id)
 );
